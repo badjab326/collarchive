a0a8d7ac83aa69063730a25a0f5d688a99e983c4
“julian <badjab326@gmail.com>
2022-05-30 11:28:27 -0700

    first commit, basic file structure esteablished

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require('express');
+const methodOverride = require("method-override");
+const app = express();
+require('dotenv').config();
+const { default: mongoose } = require("mongoose");
+
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+// Database Connection Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.static('style'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// Listener
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
