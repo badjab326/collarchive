f0e583d09c858f85bbd29387d3a3d274cfd23eb6
“julian <badjab326@gmail.com>
2022-06-02 10:14:09 -0700

    Added more seed data, updated styling for index and show pages

--- models/collectionSeed.js
+++ models/collectionSeed.js
@@ -2,7 +2,7 @@ module.exports = [{
     name: 'Battletoads',
     description: "A beat 'em up/platform video game developed by Rare and published by Tradewest.",
     img: 'https://upload.wikimedia.org/wikipedia/en/e/e8/Battletoads_Coverart.png',
-    platform: 'Nintendo',
+    platform: 'Nintendo Entertainment System',
     qty: 99
   }, {
     name: 'Battletoads in Battlemaniacs',
@@ -12,7 +12,7 @@ module.exports = [{
     qty: 1
   }, {
     name: 'Battletoads and Double Dragon',
-    description: "a crossover of both Technos Japan's Double Dragon and Rare's own Battletoads game franchises",
+    description: "A crossover of both Technos Japan's Double Dragon and Rare's own Battletoads game franchises",
     img: 'https://www.retroplace.com/pics/genesis/packshots/35959--battletoads-double-dragon.png',
     platform: 'Sega Genesis',
     qty: 1
@@ -20,19 +20,25 @@ module.exports = [{
     name: 'Grand Theft Auto: Vice City',
     description: "Grand Theft Auto: Vice City is a 2002 action-adventure game developed by Rockstar North and published by Rockstar Games.",
     img: 'https://m.media-amazon.com/images/I/6142CATEDWL._SY445_.jpg',
-    platform: 'Playstation 2',
+    platform: 'Sony Playstation 2',
     qty: 1
   }, {
-    name: 'Battletoads and Double Dragon',
-    description: "a crossover of both Technos Japan's Double Dragon and Rare's own Battletoads game franchises",
-    img: 'https://www.retroplace.com/pics/genesis/packshots/35959--battletoads-double-dragon.png',
-    platform: 'Sega Genesis',
+    name: 'Hotline Miami Collection',
+    description: "Hotline Miami collection contains both legendary games in the neon-soaked, brutally-challenging Hotline Miami series from dennaton games.",
+    img: 'https://m.media-amazon.com/images/I/71d6esPMq+S._SL1289_.jpg',
+    platform: 'Sony Playstation 4',
     qty: 1
   }, {
-    name: 'Battletoads and Double Dragon',
-    description: "a crossover of both Technos Japan's Double Dragon and Rare's own Battletoads game franchises",
-    img: 'https://www.retroplace.com/pics/genesis/packshots/35959--battletoads-double-dragon.png',
-    platform: 'Sega Genesis',
+    name: 'Batman Arkham City',
+    description: "Batman: Arkham City is a 2011 action-adventure game developed by Rocksteady Studios and published by Warner Bros. Interactive Entertainment. Based on the DC Comics superhero Batman, it is the sequel to the 2009 video game Batman: Arkham Asylum and the second installment in the Batman: Arkham series.",
+    img: 'https://m.media-amazon.com/images/M/MV5BZDE2ZDFhMDAtMDAzZC00ZmY3LThlMTItMGFjMzRlYzExOGE1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg',
+    platform: 'PC',
+    qty: 1
+  }, {
+    name: 'Marvel vs. Capcom',
+    description: "Batman: Arkham City is a 2011 action-adventure game developed by Rocksteady Studios and published by Warner Bros. Interactive Entertainment. Based on the DC Comics superhero Batman, it is the sequel to the 2009 video game Batman: Arkham Asylum and the second installment in the Batman: Arkham series.",
+    img: 'https://pbs.twimg.com/media/CatiZP0WwAAVDFg.jpg',
+    platform: 'Sega Dreamcast',
     qty: 1
   }
 ]
